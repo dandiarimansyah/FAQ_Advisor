4ad7fd7f09e5a74679e5622eb6457d4508c58c52
Dandi Arimansyah <dandimansyah@gmail.com>
2021-09-07 12:49:39 +0700

    Tambah

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -16,31 +16,36 @@
             </form>
         </div>
     </div>
-    <center>
-        <div id="kumpulan_faqs">
-            @forelse ($pertanyaan as $p)
-                <div id="tampil_pertanyaan">
-                    <h4>{{ $p-> pertanyaan }}</h4>
-                </div>
-                <div id="tampil_jawaban">
-                    @if ($p->kategori != null)
-                        <p>Kategori :
-                                @foreach ($p->kategori as $idx => $item)
-                                    @if (!isset($p->kategori[$idx+1]))
-                                        {{$item->kategori}}      
-                                    @else
-                                        {{$item->kategori}},
-                                    @endif
-                                @endforeach
-                            <br>
-                            {{ substr($p->jawaban, 0, 250) }}... <a href="{{ url('/lihat_pertanyaan/'. $p->id) }}"> Baca Selengkapnya</a>
-                        </p>
-                    @endif
-                </div>
-            @empty
-                <h2>TIDAK ADA DATA</h2>
-            @endforelse              
+
+    <div id="kumpulan_faqs">
+        @forelse ($pertanyaan as $p)
+        <div id="setiap_faqs">
+            <div id="tampil_pertanyaan">
+                <h4>{{ $p-> pertanyaan }}</h4>
+                @if ($p->kategori != null)
+                    <h6>Kategori :
+                        @foreach ($p->kategori as $idx => $item)
+                            @if (!isset($p->kategori[$idx+1]))
+                                {{$item->kategori}}      
+                            @else
+                                {{$item->kategori}},
+                            @endif
+                        @endforeach
+                    </h6>
+                @endif
+            </div>
+            <div id="tampil_jawaban">
+                <p>
+                    {{ substr($p->jawaban, 0, 250) }}... 
+                </p>
+            </div>
+            <div id="selengkapnya">
+                <a href="{{ url('/lihat_pertanyaan/'. $p->id) }}"> Baca Selengkapnya</a>
+            </div>
         </div>
-    </center>
+        @empty
+            <h2>TIDAK ADA DATA</h2>
+        @endforelse              
+    </div>
 
 @endsection
--- resources/views/pertanyaan_lihat.blade.php
+++ resources/views/pertanyaan_lihat.blade.php
@@ -19,6 +19,33 @@
         p{
             text-align: justify
         }
+        .konten{
+            display: flex;
+            gap: 20px;
+            height: 100%;
+        }
+
+        .kiri{
+            width: 80%;
+        }
+
+        .kanan{
+            width: 20%;
+        }
+
+        ul{
+            padding: 0;
+            list-style: none;
+            margin-left: 10px;
+        }
+
+        button{
+            cursor: pointer;
+        }
+
+        button:hover{
+            background-color: rgb(155, 155, 155);
+        }
     </style>
 @endsection
 
@@ -26,31 +53,45 @@
 
 <a href="{{ url()->previous() }}" style="margin-left: 0" class="back">Kembali</a>
 
-<div class="kotak">
-    <div id="judul">
-        <h2>{{$pertanyaan->pertanyaan}}</h2>
-        
-        @if ($kategori_terpilih != null)
-        <h4>Kategori :
-            @foreach ($kategori_terpilih as $idx => $item)
-                @if (!isset($kategori_terpilih[$idx+1]))
-                    {{$item->kategori}}      
-                @else
-                    {{$item->kategori}},
-                @endif
-                
-            @endforeach
-        </h4>
-        @endif
-    </div>
-    <p>{{$pertanyaan->jawaban}}</p>
-
-    <div id="bawah" style="text-align: center">
-        <h5>Apakah anda terbantu?</h5>
-        <div class="flex flex-center">
-            <button>Ya</button>
-            <button>Tidak</button>
+<div class="konten">
+    
+    <div class="kotak kiri">
+        <div id="judul">
+            <h2>{{$pertanyaan->pertanyaan}}</h2>
+            
+            @if ($kategori_terpilih != null)
+            <h4>Kategori :
+                @foreach ($kategori_terpilih as $idx => $item)
+                    @if (!isset($kategori_terpilih[$idx+1]))
+                        {{$item->kategori}}      
+                    @else
+                        {{$item->kategori}},
+                    @endif
+                    
+                @endforeach
+            </h4>
+            @endif
         </div>
+        <p>{{$pertanyaan->jawaban}}</p>
+
+        <div id="bawah" style="text-align: center">
+            <h5>Apakah anda terbantu?</h5>
+            <div class="flex flex-center">
+                <button>Ya</button>
+                <button>Tidak</button>
+            </div>
+        </div>
+    </div>
+
+    <div class="kotak kanan">
+        <h5>Referensi Pertanyaan</h5>
+        <ul>
+            <li>1. Ini dia pertanyaan</li>
+            <li>2. Ini dia pertanyaan</li>
+            <li>3. Ini dia pertanyaan</li>
+            <li>4. Ini dia pertanyaan</li>
+            <li>5. Ini dia pertanyaan</li>
+        </ul>
     </div>
 </div>
 
