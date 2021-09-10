ce3e2e0a96577272c7b8a1bf7ba667d34a01ce55
Dandi Arimansyah <dandimansyah@gmail.com>
2021-09-10 15:29:32 +0700

    Pencarian Biasa

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -13,6 +13,32 @@ class AdminController extends Controller
         $pertanyaan = Pertanyaan::orderBy('updated_at', 'desc')->get();
         $kategori = Kategori::orderBy('kategori', 'asc')->get();
 
-        return view('index', compact('pertanyaan', 'kategori'));
+        $status = true;
+        $faq = null;
+
+        if (request('search')) {
+
+            $faq = Pertanyaan::latest();
+
+            if (request('pilih_kategori')) {
+                // where kategori adalah pilih_kategori
+                // $faq = Pertanyaan::whereIn('kategori_id', 'pilih_kategori')->get();
+                // dd($faq);
+            }
+
+            $faq->where('pertanyaan', 'like', '%' . request('search') . '%')
+                ->orWhere('jawaban', 'like', '%' . request('search') . '%');
+
+
+            $faq = $faq->get();
+
+            if ($faq->count() == 0) {
+                $faq = Pertanyaan::latest();
+                $status = false;
+            }
+        }
+
+
+        return view('index', compact('pertanyaan', 'kategori', 'faq', 'status'));
     }
 }
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -8,66 +8,72 @@
             <h1>Riwayat FAQ</h1>
         </div>
         <div>
-            <form action="">
+            <form action="/">
                 <div id="form_search">
-                    <input id="search" type="search" placeholder="Masukkan Kata Kunci">
-                    <button id="cari">CARI</button>
+                    <input name="search" id="search" type="search" value="{{request('search')}}" placeholder="Masukkan Kata Kunci">
+                    <button type="submit" id="cari">CARI</button>
                 </div>
-            </form>
-            <div class="filter">
-                <h6>Filter Pencarian 
-                    <span>
-                        <i class="fa fa-angle-double-down" aria-hidden="true"></i>
-                    </span>
-                </h6>
-            </div>
-            <div class="box-filter">
-                @foreach ($kategori as $k)
+                <div class="filter">
+                    <h6>Filter Pencarian 
+                        <span>
+                            <i class="fa fa-angle-double-down" aria-hidden="true"></i>
+                        </span>
+                    </h6>
+                </div>
+                <div class="box-filter">
+                    @foreach ($kategori as $k)
                     <div>
-                        <input type="checkbox" id="{{ $k->id }}">
+                        <input type="checkbox" id="{{ $k->id }}" name="pilih_kategori[]" value="{{ $k->id }}">
                         <label for="{{ $k->id }}">{{ $k->kategori}}</label>
                     </div>
-                @endforeach
-            </div>
+                    @endforeach
+                </div>
+            </form>
         </div>
     </div>
 
     <div id="kumpulan_faqs">
-        @forelse ($pertanyaan as $p)
-        <div id="setiap_faqs">
-            <div id="{{ $p->id }}" class="tampil_pertanyaan">
-                <div>
-                    <h5>{{ $p-> pertanyaan }}</h5>
-                    @if ($p->kategori != null)
-                    <h6>Kategori :
-                        @foreach ($p->kategori as $idx => $item)
-                        @if (!isset($p->kategori[$idx+1]))
-                        {{$item->kategori}}      
-                        @else
-                        {{$item->kategori}},
-                        @endif
-                        @endforeach
-                    </h6>
-                    @endif
-                </div>
-                <div class="tombol-kanan">
-                    <i class="fa fa-caret-right" aria-hidden="true"></i>
-                </div>
-            </div>
+        @if ($status)
+            @if ($faq != null)
+                @foreach ($faq as $p)                
+                <div id="setiap_faqs">
+                    <div id="{{ $p->id }}" class="tampil_pertanyaan">
+                        <div>
+                            <h5>{{ $p-> pertanyaan }}</h5>
+                            @if ($p->kategori != null)
+                            <h6>Kategori :
+                                @foreach ($p->kategori as $idx => $item)
+                                    @if (!isset($p->kategori[$idx+1]))
+                                    {{$item->kategori}}      
+                                    @else
+                                    {{$item->kategori}},
+                                    @endif
+                                @endforeach
+                            </h6>
+                            @endif
+                        </div>
+                        <div class="tombol-kanan">
+                            <i class="fa fa-caret-right" aria-hidden="true"></i>
+                        </div>
+                    </div>
 
-            <div id="jawaban{{ $p->id }}" class="tampil_jawaban">
-                <p>
-                    {!! substr($p->jawaban, 0, 250) !!}... 
-                </p>
-                <div id="selengkapnya">
-                    <a href="{{ url('/lihat_faq/'. $p->id) }}"> Baca Selengkapnya</a>
-                </div>
-            </div>
+                    <div id="jawaban{{ $p->id }}" class="tampil_jawaban">
+                        <p>
+                            {!! substr($p->jawaban, 0, 250) !!}... 
+                        </p>
+                        <div id="selengkapnya">
+                            <a href="{{ url('/lihat_faq/'. $p->id) }}"> Baca Selengkapnya</a>
+                        </div>
+                    </div>
 
-        </div>
-        @empty
-            <h2>TIDAK ADA DATA</h2>
-        @endforelse              
+                </div>
+                @endforeach
+            @endif
+
+        @else
+            <h2 style="color: white" class="tengah">TIDAK ADA DATA</h2>        
+        @endif
+
     </div>
 
 @endsection
@@ -82,7 +88,6 @@
     $(document).on("click",".filter", function () {
         $('.box-filter').toggle(30);
     });
-
 
     $(document).on("click",".tampil_pertanyaan", function () {
         var id = $(this).attr('id');
